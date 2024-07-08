470b527bffc479862875c8f1c218446cba072590
Robert Smith <smith.rob@student.greenriver.edu>
2024-07-07 20:40:17 -0700

    Writing the interface. jqs will exist as the counterpart to qml in pennylane that accepts the users inputs and performs the interpreting.
    CommandParser will be the layer that receives the user input from the front end and passes it to qml.
    
    TODO: Build ResponseParser to send from the system to the front end.

---
+++ src/interpreter/jqs.java
@@ -0,0 +1,316 @@
+package interpreter;
+
+import state.StateTracker;
+import state.WorkItem;
+import state.WorkQueue;
+
+/**
+ * This class provides the user interface for jqsim. The commands are similar to other established
+ * quantum simulator libraries.
+ * <br>
+ * The jqs constructor is overloaded, you can provide the number of shots as the 2nd parameter if you
+ * desire a different number of shots than the default of 1024.
+ * <br><ul>
+ * Phi values: as a double.
+ * Control values: as an integer of which qubit to use as control.
+ * Target values: as an integer of which qubit to use as the target.
+ * <br>
+ * For multi-qubit gates, provide an array of int for controls and an array of int[] as targets.
+ * </ul>
+ * <br>This class instantiates a StateTracker which tracks the system vector as the system state as a column vector
+ * of the qubit amplitudes. It also instantiates the WorkQueue which processes the gates applied to each qubit as the
+ * user defines.
+ *
+ * @author Robert Smith
+ * @version 0.1
+ * @since 7 July 2024
+ */
+public class jqs {
+    private static int numShots = 1024;
+    private static WorkQueue workQueue;
+
+    /**
+     * Initializes the jqs device with the specified number of qubits and default number of shots.
+     *
+     * @param numberOfQubits The number of qubits for the device.
+     */
+    public void device(int numberOfQubits) {
+        workQueue = new WorkQueue();
+        StateTracker tracker = new StateTracker(numberOfQubits);
+    }
+
+    /**
+     * Initializes the jqs device with the specified number of qubits and number of shots.
+     *
+     * @param numberOfQubits The number of qubits for the device.
+     * @param numShots       The number of shots to run the simulation.
+     */
+    public void device(int numberOfQubits, int numShots) {
+        jqs.numShots = numShots;
+        workQueue = new WorkQueue();
+        StateTracker tracker = new StateTracker(numberOfQubits);
+    }
+
+    ///////////////////////////////////
+    // Single Qubit Gates //
+    ///////////////////////////////////
+
+    /**
+     * Applies the RZ gate to the specified target qubit.
+     *
+     * @param phi    The rotation angle in radians.
+     * @param target The target qubit.
+     */
+    public void RZ(double phi, int target) {
+        workQueue.addGate(new WorkItem("RZ", target));
+    }
+
+    /**
+     * Applies the RX gate to the specified target qubit.
+     *
+     * @param phi    The rotation angle in radians.
+     * @param target The target qubit.
+     */
+    public void RX(double phi, int target) {
+        workQueue.addGate(new WorkItem("RX", target));
+    }
+
+    /**
+     * Applies the RY gate to the specified target qubit.
+     *
+     * @param phi    The rotation angle in radians.
+     * @param target The target qubit.
+     */
+    public void RY(double phi, int target) {
+        workQueue.addGate(new WorkItem("RY", target));
+    }
+
+    /**
+     * Applies the S gate to the specified target qubit.
+     *
+     * @param target The target qubit.
+     */
+    public void S(int target) {
+        workQueue.addGate(new WorkItem("S", target));
+    }
+
+    /**
+     * Applies the T gate to the specified target qubit.
+     *
+     * @param target The target qubit.
+     */
+    public void T(int target) {
+        workQueue.addGate(new WorkItem("T", target));
+    }
+
+    /**
+     * Applies the Ti gate to the specified target qubit.
+     *
+     * @param target The target qubit.
+     */
+    public void Ti(int target) {
+        workQueue.addGate(new WorkItem("Ti", target));
+    }
+
+
+    /**
+     * Applies the IDENTITY gate to the specified target qubit.
+     *
+     * @param target The target qubit.
+     */
+    public void ID(int target) {
+        workQueue.addGate(new WorkItem("ID", target));
+    }
+
+    /**
+     * Applies the HADAMARD gate to the specified target qubit.
+     *
+     * @param target The target qubit.
+     */
+    public void H(int target) {
+        workQueue.addGate(new WorkItem("H", target));
+    }
+
+    /**
+     * Applies the Si gate to the specified target qubit (S INVERSE).
+     *
+     * @param target The target qubit.
+     */
+    public void Si(int target) {
+        workQueue.addGate(new WorkItem("SX", target));
+    }
+
+    ///////////////////////////////////
+    // Dual Qubit Gates //
+    ///////////////////////////////////
+
+    /**
+     * Applies the CZ (controlled Z) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CZ(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("CZ", controls, targets));
+    }
+
+    /**
+     * Applies the CY (controlled Y) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CY(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("CY", controls, targets));
+    }
+
+    /**
+     * Applies the CNOT (controlled X) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CNOT(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("CNOT", controls, targets));
+    }
+
+    /**
+     * Applies the CH (controlled HADAMARD) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CH(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("CH", controls, targets));
+    }
+
+    /**
+     * Applies the SWAP gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void SWAP(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("SWAP", controls, targets));
+    }
+
+    /**
+     * Applies the ISWAP (inverse SWAP) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void ISWAP(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("ISWAP", controls, targets));
+    }
+
+    /**
+     * Applies the CSWAP (controlled SWAP) gate with the specified control and target qubits.
+     *
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CSWAP(int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("CSWAP", controls, targets));
+    }
+
+    ///////////////////////////////////
+    // Multi Qubit Gates //
+    ///////////////////////////////////
+
+    /**
+     * Applies the Toffoli (controlled controlled X) gate with the specified control qubits and target qubit.
+     *
+     * @param controlOne The first control qubit.
+     * @param controlTwo The second control qubit.
+     * @param target     The target qubit.
+     */
+    public void Toffoli(int controlOne, int controlTwo, int target) {
+        Integer[] controls = {controlOne, controlTwo};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem("Toffoli", controls, targets));
+    }
+
+    /**
+     * Applies a controlled gate with the specified gate name, control qubit, and target qubit.
+     *
+     * Single control and single target gate
+     * Accepts any single qubit gate as the type to apply as controlled gate, e.g. cS, cT etc.
+     *
+     * @param gate    The name of the controlled gate.
+     * @param control The control qubit.
+     * @param target  The target qubit.
+     */
+    public void CGate(String gate, int control, int target) {
+        Integer[] controls = {control};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem(gate, controls, targets));
+    }
+
+    /**
+     * Applies a controlled gate with the specified gate name, control qubits, and target qubits.
+     *
+     * Multi control and multi target, can be a single control or many, and a single target or many.
+     * Accepts any single qubit gate as the type to apply as controlled gate, e.g. cS, cT etc.
+     *
+     * @param gate     The name of the controlled gate.
+     * @param controls The array of control qubits.
+     * @param targets  The array of target qubits.
+     */
+    public void CGate(String gate, int[] controls, int[] targets) {
+        Integer[] controlsInteger = new Integer[controls.length];
+        for (int i = 0; i < controls.length; i++) {
+            controlsInteger[i] = controls[i];
+        }
+        Integer[] targetsInteger = new Integer[targets.length];
+        for (int i = 0; i < targets.length; i++) {
+            targetsInteger[i] = targets[i];
+        }
+        workQueue.addGate(new WorkItem(gate, controlsInteger, targetsInteger));
+    }
+
+    /**
+     * Applies a controlled-controlled gate with the specified gate name, control qubits, and target qubit.
+     *
+     * Dual control single target gate.
+     * Accepts any single qubit gate as the type to apply as controlled gate, e.g. cS, cT etc.
+     *
+     * @param gate       The name of the controlled-controlled gate.
+     * @param controlOne The first control qubit.
+     * @param controlTwo The second control qubit.
+     * @param target     The target qubit.
+     */
+    public void CCGate(String gate, int controlOne, int controlTwo, int target) {
+        Integer[] controls = {controlOne, controlTwo};
+        Integer[] targets = {target};
+        workQueue.addGate(new WorkItem(gate, controls, targets));
+    }
+
+
+    ///////////////////////////////////
+    // System Results //
+    ///////////////////////////////////
+
+    /**
+     * Calculates and returns the expected value of the quantum system.
+     *
+     * @return The expected value.
+     */
+    public void expval() {
+        // TODO: Implement expval method
+    }
+}
