0e6e1d4de4a74f9f93bc78e9a311dd795d8ae313
Robert Smith <smith.rob@student.greenriver.edu>
2024-08-02 12:52:24 -0700

    Added tests for QFTi with quirk references.

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -26,20 +26,12 @@ public class Sandbox {
      * @param args none needed or accounted for.
      */
     public static void main(String[] args) {
-        jqs jqs = new jqs(3);
-        jqs.X(0);
-        jqs.S(0);
-        jqs.T(0);
-//        jqs.RX(Math.PI/5, 0);
-        jqs.X(1);
-        jqs.T(1);
-        jqs.X(2);
-//        jqs.RY(3*Math.PI/2, 2);
-        jqs.getComputationalState();
-        System.out.println("Starting State: " + jqs);
+        jqs jqs = new jqs(6);
+
         jqs.QFT();
-        System.out.println("\nQFT State: "+ jqs);
+
+        System.out.println(jqs);
         jqs.QFTi();
-        System.out.println("\nAfter QFTi" +jqs);
+        System.out.println(jqs);
     }
 }
